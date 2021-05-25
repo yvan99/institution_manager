19880954fb5ea8538b6858da392e45e1a72c3bac
yvan99 <ishimweyvan90@gmail.com>
2021-05-25 15:48:54 +0200

    includes files

---
+++ inc/js.php
@@ -0,0 +1,15 @@
+  <!-- Vendor Scripts -->
+  <script src="assets/landing/js/vendor.min.js"></script>
+  <!-- Plugin's Scripts -->
+  <script src="assets/landing/plugins/fancybox/jquery.fancybox.min.js"></script>
+  <script src="assets/landing/plugins/nice-select/jquery.nice-select.min.js"></script>
+  <script src="assets/landing/plugins/aos/aos.min.js"></script>
+  <script src="assets/landing/plugins/slick/slick.min.js"></script>
+  <script src="../../../porjoton.netlify.app/mekanic/js/waypoints.min.js"></script>
+  <script src="assets/landing/plugins/counter-up/jquery.counterup.min.js"></script>
+  <script src="assets/landing/plugins/isotope/isotope.pkgd.min.js"></script>
+  <script src="assets/landing/plugins/isotope/packery.pkgd.min.js"></script>
+  <script src="assets/landing/plugins/isotope/image.loaded.js"></script>
+  <script src="assets/landing/plugins/menu/menu.js"></script>
+  <!-- Activation Script -->
+  <script src="assets/landing/js/custom.js"></script>
