15e386ba85fe1b3a259a72806f68b9985ecc44bf
yvan99 <ishimweyvan90@gmail.com>
2021-05-25 15:51:53 +0200

    index file

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php require_once 'inc/css.php'; ?>
+</head>
+
+<body data-theme-mode-panel-active data-theme="light" style="font-family: 'Mazzard H';">
+  <div class="site-wrapper overflow-hidden position-relative">
+    <!-- Site Header -->
+    <!--Site Header Area -->
+    <header class="site-header site-header--menu-left landing-11-menu site-header--absolute site-header--sticky">
+      <div class="container">
+        <nav class="navbar site-navbar">
+          <?php require_once 'inc/navbar.php'; ?>
+        </nav>
+      </div>
+    </header>
+    <!-- navbar- -->
+    <!-- Hero Area -->
+    <div class="hero-area-l11 position-relative z-index-1 overflow-hidden">
+      <div class="container position-relative">
+        <div class="row position-relative justify-content-center">
+          <div class="col-xl-8 col-lg-9 col-md-12 order-lg-1 order-1">
+            <div class="content">
+              <h1>Institute<br class="d-none d-md-block"> Management software.</h1>
+              <div class="row banner-l-11-bottom-content">
+                <div class="col-lg-8 col-md-8 col-sm-10">
+                  <p class="position-relative banner-main-content-l-11">Create custom landing pages with
+                    Shades that convert more
+                    visitors
+                    than any website—no coding required.
+                    <span class="line-left-content"></span>
+                  </p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 flex-y-center ">
+                  <a class="btn l-11-hero-btn" href="#">Download free trial</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="hero-shape-l11-1 d-none d-md-block">
+        <img src="assets/landing/image/l2/hero-shape-1.svg" alt="">
+      </div>
+      <div class="hero-shape-l11-2 d-none d-md-block">
+        <img src="assets/landing/image/l2/hero-shape-2.png" alt="">
+      </div>
+    </div>
+
+    <!-- Content Area-1 -->
+    <div class="content-area-l-11-1">
+      <div class="container">
+        <div class="row align-items-center justify-content-lg-start justify-content-center">
+          <div class="col-xl-6 col-md-5 col-md-10" data-aos="fade-right" data-aos-duration="800" data-aos-once="true">
+            <div class="content-img position-relative z-index-1">
+              <img src="assets/landing/image/l2/content-img1.png" alt="" class="w-100">
+            </div>
+          </div>
+          <div class="offset-xxl-1 col-xxl-5 col-xl-6 col-lg-7 col-md-9" data-aos="fade-left" data-aos-duration="800" data-aos-once="true">
+            <div class="content section-heading-5">
+              <h2>
+                Build Beautiful Landing Pages Faster.
+              </h2>
+              <p>Create custom landing pages with Shades that convert more visitors than any website, no coding
+                required.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Content Area-2  -->
+    <div class="content-area-l-11-2">
+      <div class="container">
+        <div class="row align-items-center justify-content-lg-between justify-content-center">
+          <div class="col-xxl-4 col-xl-5 col-lg-6 col-md-9 order-lg-1 order-1" data-aos="fade-right" data-aos-duration="800" data-aos-once="true">
+            <div class="content section-heading-5">
+              <h2>
+                Completely Free for Everyone.
+              </h2>
+              <div class="d-flex content-l-11-3-card">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="27" viewBox="0 0 32 27">
+                  <g>
+                    <g>
+                      <g>
+                        <path class="mypath1" fill="none" stroke="#258aff" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="20" stroke-width="2" d="M25.528 25.615v0h5.47v0-5.911c0-.56-.341-1.062-.86-1.27l-5.118-2.05a1.367 1.367 0 0 1-.86-1.267v-1.2a5.445 5.445 0 0 0 2.735-4.711V6.471a5.47 5.47 0 0 0-8.205-4.738">
+                        </path>
+                      </g>
+                      <g>
+                        <path class="mypath1" fill="none" stroke="#258aff" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="20" stroke-width="2" d="M19.367 19.846l-5.14-2.036a1.358 1.358 0 0 1-.864-1.257v-1.192a5.394 5.394 0 0 0 2.747-4.676V7.97c0-2.999-2.46-5.43-5.495-5.43-3.034 0-5.494 2.431-5.494 5.43v2.715a5.394 5.394 0 0 0 2.747 4.676v1.192c0 .555-.342 1.054-.864 1.26l-5.14 2.036c-.52.206-.863.703-.864 1.257v4.51h19.23v0-4.51c0-.555-.342-1.054-.863-1.26z">
+                        </path>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+                <div class="content-body">
+                  <h5>Easy setup process</h5>
+                  <p>Editing and customizing Essential Landing is easy and fast.</p>
+                </div>
+              </div>
+              <div class="d-flex content-l-11-3-card">
+                <img src="assets/landing/image/l2/settings.svg" alt="icon">
+                <div class="content-body">
+                  <h5>Built for real users</h5>
+                  <p>Editing and customizing Essential Landing is easy and fast.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-xxl-6 offset-xxl-2 col-xl-6 col-lg-5 col-md-9 order-lg-1 order-0" data-aos="fade-left" data-aos-duration="800" data-aos-once="true">
+            <div class="content-img">
+              <img src="assets/landing/image/l2/content-img2.png" alt="" class="w-lg-auto w-100">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Content Area -->
+    <div class="content-area-l-11-3 position-relative">
+      <div class="container">
+        <div class="row align-items-center justify-content-center justify-content-lg-start">
+          <div class="col-xl-6 col-lg-6 col-md-8 order-lg-1 order-0" data-aos="fade-up" data-aos-delay="500" data-aos-duration="1000">
+            <div class="content-img">
+              <img src="assets/landing/image/l2/content-img3.png" alt="" class="w-100">
+            </div>
+          </div>
+          <div class="offset-xl-1 col-xl-5 col-lg-6 col-md-9 order-lg-1 order-1" data-aos="fade-up" data-aos-delay="300" data-aos-duration="1000">
+            <div class="content section-heading-5">
+              <h2>Completely Free for Everyone.</h2>
+              <p>Event is not like most tech conferences. We want our presentations to engage the audience, spark
+                discussion and inspire new ideas. </p>
+              <ul class="list-unstyled pl-0">
+                <li class="d-flex align-items-center">
+                  <i class="fas fa-check"></i>Unlimited design possibility
+                </li>
+                <li class="d-flex align-items-center">
+                  <i class="fas fa-check"></i>Completely responsive
+                </li>
+                <li class="d-flex align-items-center">
+                  <i class="fas fa-check"></i>Easy to customize
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php require_once 'inc/footer.php' ?>
+  </div>
+  <?php require_once 'inc/js.php' ?>
+</body>
+
+</html>
