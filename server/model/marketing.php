9102a79d9d2abca569026338b9f7f57a8c905a4a
mitali-olivier <oliviermitali3@gmail.com>
2021-05-26 09:55:47 +0300

    added marketing file

---
+++ server/model/marketing.php
@@ -0,0 +1,54 @@
+<?php
+require_once 'server/init.php';
+class Marketing
+{
+    public $tableName = "advertisment";
+
+    function __construct()
+    {
+        $this->tableName;
+    }
+
+    function insertAdvert($name, $description, $user_id)
+    {
+        $conn = db();
+        $query = select("*", $this->tableName, "name='$name'");
+        if (!$query) {
+            $insertAd = insert($this->tableName, '`ads_id`, `name`, `description`, `user_id`', "NULL,$name,$description,$user_id");
+            return "advert creation successful";
+        } else {
+            return "advert exits";
+        }
+    }
+
+    function deleteAdvert($ads_id)
+    {
+        $conn = db();
+        $deleteAdvert = delete($this->tableName, "ads_id=$ads_id");
+        if ($deleteAdvert) {
+            return "Advert deleted!";
+        } else {
+            return "could not delete! Contact Admin";
+        }
+    }
+
+    function updateAdvert($ads_id, $name, $description, $user_id)
+    {
+        $updateAdvert = update($this->tableName, "name=$name,description=$description", "ads_id=$ads_id");
+        if ($updateAdvert) {
+            return "Advert Updated!";
+        } else {
+            return "Could not update! Contact Admin";
+        }
+    }
+
+    function displayAdvert()
+    {
+        $selectAdvert = select("*", $this->tableName, "");
+        if ($selectAdvert) {
+            return $selectAdvert;
+        } else {
+            return "No results to display! Add atleast one entry";
+        }
+    }
+}
