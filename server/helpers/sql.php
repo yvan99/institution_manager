d8875691758941974ac2221bfe70fed710638713
prince <nshutiprinceberyl@gmail.com>
2021-05-26 09:40:49 +0200

    created model and sql

--- server/helpers/sql.php
+++ server/helpers/sql.php
@@ -1,10 +1,31 @@
-<?php 
+<?php
 require_once '../config/connect.php';
-function select($data,$table,$condition){
-$conn = db();
-$selector = mysqli_query($conn,"SELECT"." ".$data." "."FROM"." ".$table." "."WHERE"." ".$condition);
-//var_dump($selector);
-$fetcher  = mysqli_fetch_array($selector);
-return $fetcher;
+function select($data, $table, $condition)
+{
+    $conn = db();
+    $selector = mysqli_query($conn, "SELECT" . " " . $data . " " . "FROM" . " " . $table . " " . "WHERE" . " " . $condition);
+    //var_dump($selector);
+    $fetcher  = mysqli_fetch_array($selector);
+    return $fetcher;
 }
 
+function insert($table, $tableStructure, $values)
+{
+    $conn = db();
+    $selector = mysqli_query($conn, "INSERT INTO " . $table . " " . ($tableStructure) . " VALUES " . ($values));
+    return $selector;
+}
+
+function delete($table, $condition)
+{
+    $conn = db();
+    $selector = mysqli_query($conn, "DELETE FROM " . $table . " WHERE " . ($condition));
+    return $selector;
+}
+
+function update($table, $column, $condition)
+{
+    $conn = db();
+    $selector = mysqli_query($conn, "UPDATE  " . $table . " SET " . ($column) . " WHERE " . ($condition));
+    return $selector;
+}
---
+++ server/model/finance.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'config/connect.php';
+class Finance
+{
+
+    public $tableName = "invoice";
+
+    function __construct()
+    {
+        $this->tableName;
+    }
+
+    function insertInvoice($name, $date, $user_id)
+    {
+        $conn = db();
+        $query = select('*', $this->tableName, "name='$name'");
+        if (!$query) {
+            $insertInvoice = insert($this->tableName, '`invoice_id`, `name`, `date`, `user_id`', "NULL,$name,$date,$user_id");
+            die("done");
+        } else {
+            return "name already used";
+        }
+    }
+
+    function deleteInvoice($invoice_id)
+    {
+        $deleteInvoice = delete($this->tableName, "invoice_id=$invoice_id");
+        if ($deleteInvoice) {
+            return "Record deleted successfully";
+        } else {
+            return  "Error deleting record: ";
+        }
+    }
+    function updateInvoice($invoice_id, $name, $date, $user_id)
+    {
+        $updateInvoice = update($this->tableName, "name=$name,date=$date", "invoice_id=$invoice_id");
+        if ($updateInvoice) {
+            return "Record updated successfully";
+        } else {
+            return "Error updating record: ";
+        }
+    }
+    function selectInvoice()
+    {
+        $selectInvoice = select("*", $this->tableName, "");
+        if ($selectInvoice) {
+            return $selectInvoice;
+        } else {
+            return  "0 results";
+        }
+    }
+}
